0cdf8dde5e3896f27e73b2f57f955d90fe7df021
HoshinoHB <yanzhihui163@gmail.com>
2026-08-13 20:10:23 +0800

    更新 siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -1,13 +1,13 @@
 import type { SiteConfig } from "../types/config";
 
 // 定义站点语言
-const SITE_LANG = "en"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
+const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 
 export const siteConfig: SiteConfig = {
-	title: "Mizuki",
-	subtitle: "One demo website",
-	siteURL: "https://mizuki.mysqil.com/", // 请替换为你的站点URL，以斜杠结尾
-	siteStartDate: "2025-01-01", // 站点开始运行日期，用于站点统计组件计算运行天数
+	title: "HoshinoHB | 个人博客",
+	subtitle: "这是一个老师的个人终端博客站点，记录网络技术折腾、代码踩坑、游戏、生活脑洞，技术随笔与日常碎碎念",
+	siteURL: "https://hoshinohb.de5.net/", // 请替换为你的站点URL，以斜杠结尾
+	siteStartDate: "2026-08-13", // 站点开始运行日期，用于站点统计组件计算运行天数
 	timeZone: "Asia/Shanghai", // 文章日期使用的 IANA 时区，可改为 Asia/Tokyo、Europe/Berlin 等
 
 	lang: SITE_LANG,
@@ -35,7 +35,7 @@ export const siteConfig: SiteConfig = {
 		// 显示模式："text-icon" 显示图标+文本，"logo" 仅显示Logo
 		mode: "text-icon",
 		// 顶栏标题文本
-		text: "MizukiUI",
+		text: "HMUI",
 		// 顶栏标题图标路径，默认使用 public/assets/home/home.webp
 		icon: "assets/home/home.webp",
 		// 网站Logo图片路径
@@ -162,15 +162,15 @@ export const siteConfig: SiteConfig = {
 
 		homeText: {
 			enable: true,
-			title: "わたしの部屋",
+			title: "HoshinoHB",
 			switchable: true,
 
 			subtitle: [
-				"特別なことはないけど、君がいると十分です",
-				"今でもあなたは私の光",
-				"君ってさ、知らないうちに私の毎日になってたよ",
-				"君と話すと、なんか毎日がちょっと楽しくなるんだ",
-				"今日はなんでもない日。でも、ちょっとだけいい日",
+				"身份认证已通过，沙勒欢迎你",
+				"和你的日常，就是奇迹",
+				"奇迹既是终点，也是起点",
+				"平凡的日常，也闪耀着光芒",
+				"与学生们，谱写属于我们的故事",
 			],
 			typewriter: {
 				enable: true, // 启用副标题打字机效果
