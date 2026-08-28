b026d16c3e693f1a0dd73cec4b345c500fa84db9
HoshinoHB <yanzhihui163@gmail.com>
2026-08-28 21:39:30 +0800

    fix:修复音乐播放器封面变量、音频根路径

--- src/components/widgets/music-player/constants.ts
+++ src/components/widgets/music-player/constants.ts
@@ -1,11 +1,9 @@
-import clCover from "../../../assets/music/cover/cl.webp?url";
-import dazbeeCover from "../../../assets/music/cover/dazbee.webp?url";
-import hitoriCover from "../../../assets/music/cover/hitori.webp?url";
-import xryxCover from "../../../assets/music/cover/xryx.webp?url";
+import readyGoCover from "../../../assets/music/cover/ready_go.webp?url";
+import unwelcomeCover from "../../../assets/music/cover/unwelcome_school.webp?url";
+import constantCover from "../../../assets/music/cover/constant_moderato.webp?url";
 import type { Song } from "./types";
 
 export const STORAGE_KEY_VOLUME = "music-player-volume";
-
 export const DEFAULT_VOLUME = 0.7;
 
 export const DEFAULT_COVER_URL = "/favicon/favicon.ico";
@@ -13,35 +11,27 @@ export const DEFAULT_COVER_URL = "/favicon/favicon.ico";
 export const LOCAL_PLAYLIST: Song[] = [
 	{
 		id: 1,
-		title: "口笛で愛は歌えない",
-		artist: "Dazbee",
-		cover: dazbeeCover,
-		url: "assets/music/url/dazbee.mp3",
+		title: "准备出发",
+		artist: "ブルーアーカイブ",
+		cover: readyGoCover,
+		url: "/assets/music/url/ready_go.mp3",
 		duration: 241,
 	},
 	{
 		id: 2,
-		title: "ひとり上手",
-		artist: "Kaya",
-		cover: hitoriCover,
-		url: "assets/music/url/hitori.mp3",
+		title: "Unwelcome School",
+		artist: "ブルーアーカイブ",
+		cover: unwelcomeCover,
+		url: "/assets/music/url/unwelcome_school.mp3",
 		duration: 253,
 	},
 	{
 		id: 3,
-		title: "眩耀夜行",
-		artist: "ス리즈ブーケ",
-		cover: xryxCover,
-		url: "assets/music/url/xryx.mp3",
+		title: "Constant Moderato",
+		artist: "ブルーアーカイブ",
+		cover: constantCover,
+		url: "/assets/music/url/constant_moderato.mp3",
 		duration: 245,
-	},
-	{
-		id: 4,
-		title: "春雷の頃",
-		artist: "22/7",
-		cover: clCover,
-		url: "assets/music/url/cl.mp3",
-		duration: 242,
 	},
 ];
 
@@ -55,10 +45,11 @@ export const DEFAULT_SONG: Song = {
 };
 
 export const DEFAULT_METING_API =
-	"https://www.bilibili.uno/api?server=:server&type=:type&id=:id&auth=:auth&r=:r";
+	"https://www.bilibili.uno/api?server=:server&type=:type";
 export const DEFAULT_METING_ID = "14164869977";
 export const DEFAULT_METING_SERVER = "netease";
 export const DEFAULT_METING_TYPE = "playlist";
 
 export const ERROR_DISPLAY_DURATION = 3000;
 export const SKIP_ERROR_DELAY = 1000;
+
