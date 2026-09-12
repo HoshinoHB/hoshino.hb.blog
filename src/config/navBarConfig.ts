3ecbd29ad7c46552453b40182a58f998fb6817cc
HoshinoHB <yanzhihui163@gmail.com>
2026-09-12 11:06:43 +0800

    小修改

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -26,7 +26,7 @@ export const navBarConfig: NavBarConfig = {
 					name: "抖音",
 					url: "https://v.douyin.com/hzJCUqzF6pE/",
 					external: true,
-					icon: "icon: "simple-icons:douyin",
+					icon: "simple-icons:douyin",
 				},
 				{
 					name: "CSDN",
